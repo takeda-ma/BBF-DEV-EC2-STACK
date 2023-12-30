f46f15a0302e54fb00d1f3ec3bddc76761d93d7a
takeda-ma <tkd.itsk@proton.me>
2023-12-30 14:57:14 +0900

    Initial commit

---
+++ test/bff-dev-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BffDevEc2 from '../lib/bff-dev-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bff-dev-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BffDevEc2.BffDevEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
