10d1c3d8bb7af8134e16044041a14d0c5d7464fc
takeda-ma <tkd.itsk@proton.me>
2023-12-30 20:57:48 +0900

    remove some files

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# Welcome to your CDK TypeScript project
+# Stack for devs redises running on ec2
 
-This is a blank project for CDK development with TypeScript.
+usage TBA
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+Run whenever there is a change in CDK
 
-## Useful commands
+```
+npm run build
+```
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+Deploy
+
+```
+cdk deploy
+```
+
+Destroy
+
+```
+cdk destroy
+```
--- test/bbf-dev-ec2.test.ts
+++ test/bbf-dev-ec2.test.ts
@@ -1,17 +1,37 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as BffDevEc2 from '../lib/bff-dev-ec2-stack';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { BbfDevEc2Stack } from '../lib/bbf-dev-ec2-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/bff-dev-ec2-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new BffDevEc2.BffDevEc2Stack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('VPC and Subnet Configuration', () => {
+  const app = new App();
+  const stack = new BbfDevEc2Stack(app, 'TestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.resourceCountIs('AWS::EC2::VPC', 1);
+
+  template.hasResourceProperties('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.0.0/24',
+    MapPublicIpOnLaunch: true
+  });
 });
+
+test('EC2 Instance Configuration', () => {
+  const app = new App();
+  const stack = new BbfDevEc2Stack(app, 'TestStack');
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::EC2::Instance', 2);
+
+  template.hasResourceProperties('AWS::EC2::Instance', {
+    InstanceType: 't2.micro',
+  });
+});
+
+test('Security Group Configuration', () => {
+  const app = new App();
+  const stack = new BbfDevEc2Stack(app, 'TestStack');
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::EC2::SecurityGroup', 1);
+});
+
